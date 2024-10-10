1ce30131b253bcc5ce0c7fc6e4fb68466805a8b5
yxb8mh <yxb8mh@virginia.edu>
2024-10-10 14:38:47 +0000

    cleanup on aisle 7

--- app.py
+++ app.py
@@ -15,4 +15,10 @@ id = response['id'] #calling the key out, the value of the id will be called out
 setup = response['setup']
 punchline = response['punchline']
 
-print(id, setup, punchline)
+#print(id, setup, punchline)
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="joke.log", encoding="utf-8", level=logging.DEBUG)
+
+logging.warning('%s:%s:%s', id, setup, punchline)
+
